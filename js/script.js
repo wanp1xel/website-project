0cecf1bf37df964015a49046826679dc78a0d279
Klerkins <vilkey2000@gmail.com>
2023-05-23 20:36:26 +0300

    пофіксив дропдаун

--- js/script.js
+++ js/script.js
@@ -25,3 +25,25 @@ window.onscroll = () => {
   let header = document.querySelector('header');
   header.classList.toggle('sticky', window.scrollY > 50);
 }
+
+// Закриття активного accordion-link при кліку на нього
+var accordionLinks = document.querySelectorAll('.accordion-link');
+
+accordionLinks.forEach(function(link) {
+  link.addEventListener('click', function(event) {
+    event.preventDefault();
+    var parentItem = this.parentNode;
+
+    if (parentItem.classList.contains('active')) {
+      parentItem.classList.remove('active');
+    } else {
+      // Закриття інших активних accordion-item
+      var activeItems = document.querySelectorAll('.accordion-item.active');
+      activeItems.forEach(function(activeItem) {
+        activeItem.classList.remove('active');
+      });
+
+      parentItem.classList.add('active');
+    }
+  });
+});
